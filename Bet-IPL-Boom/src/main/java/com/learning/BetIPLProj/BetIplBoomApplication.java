f697aa93e37d22241656846e257c3d3cd7920f99
ratul <ratul@192.168.0.102>
2018-03-03 03:34:09 +0530

    First Commit - Full Project

---
+++ Bet-IPL-Boom/src/main/java/com/learning/BetIPLProj/BetIplBoomApplication.java
@@ -0,0 +1,12 @@
+package com.learning.BetIPLProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetIplBoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetIplBoomApplication.class, args);
+	}
+}
