1876271e1185bac55e56a753b94466034e8af268
ratul <ratul@192.168.0.102>
2018-03-07 21:08:30 +0530

    View Resolver Changes Committed

--- Bet-IPL-Boom/src/main/java/com/learning/BetIPL/controller/LoginController.java
+++ Bet-IPL-Boom/src/main/java/com/learning/BetIPL/controller/LoginController.java
@@ -1,17 +1,17 @@
 package com.learning.BetIPL.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.learning.BetIPL.service.LoginService;
 
-@RestController
-@SessionAttributes
+@Controller
+@SessionAttributes("name")
 public class LoginController {
 	
 	@Autowired
@@ -19,7 +19,7 @@ public class LoginController {
 	
 	@RequestMapping(value="/login", method = RequestMethod.GET)
     public String showLoginPage(ModelMap model){
-        return "login";
+        return "index";
     }
     @RequestMapping(value="/login", method = RequestMethod.POST)
     public String showWelcomePage(ModelMap model, @RequestParam String name, @RequestParam String password){
